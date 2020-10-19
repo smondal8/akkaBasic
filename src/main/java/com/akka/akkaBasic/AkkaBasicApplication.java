ed7b36317751b103c2296f9a5948dba5530286f2
smondal8 <54061876+smondal8@users.noreply.github.com>
2020-10-19 18:00:49 +0530

    First commit

---
+++ src/main/java/com/akka/akkaBasic/AkkaBasicApplication.java
@@ -0,0 +1,13 @@
+package com.akka.akkaBasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkkaBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkkaBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akka/akkaBasic/AkkaBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akka.akkaBasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkkaBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
